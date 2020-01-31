923f7f9802e2484febe0504f2f3dbd4342d37b80
jmtaborda <jmtaborda@stratio.com>
2020-01-31 14:33:12 +0100

    Prueba Runner

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
     features = "src/test/resources/features",
-        tags = {"@PruebaOutline"},
+        tags = {"@Test"},
         glue = {"stepsDefinition"}
 )
 public class RunnerTest {
